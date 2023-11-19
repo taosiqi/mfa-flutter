68ac5dc4650bfcfedd4e021e406b16782cb7cb17
陶思淇 <thinklf97@gmail.com>
2023-11-19 20:54:39 +0800

    feat: 删除无用路由

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,32 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:mfa/pages/demo/provider.dart';
-import 'package:mfa/pages/entry/entry.dart';
-import 'package:mfa/pages/entry/scan.dart';
-import 'package:mfa/pages/export/after.dart';
-import 'package:mfa/pages/export/code.dart';
-import 'package:mfa/pages/export/export.dart';
 import 'package:mfa/pages/home.dart';
-import 'package:mfa/pages/import/guide.dart';
-import 'package:mfa/pages/import/import.dart';
-import 'package:mfa/pages/import/offline.dart';
 import 'package:mfa/pages/login.dart';
-import 'package:mfa/pages/poster.dart';
-import 'package:mfa/pages/setting/privacy.dart';
-import 'package:mfa/pages/setting/setting.dart';
 
 final Map<String, WidgetBuilder> routes = {
   "/": (context) => const HomePage(), //注册首页路由
   'provide': (context) => const ProvidePage(),
   "login": (context) => const LoginPage(),
-  "entry": (context) => const EntryPage(),
-  "scan": (context) => const ScanPage(),
-  "export": (context) => const ExportPage(),
-  "code": (context) => const CodePage(),
-  "after": (context) => const AfterPage(),
-  "import": (context) => const ImportPage(),
-  "guide": (context) => const GuidePage(),
-  "offline": (context) => const OfflinePage(),
-  "setting": (context) => const SettingPage(),
-  "privacy": (context) => const PrivacyPage(),
-  "poster": (context) => const PosterPage(),
 };
